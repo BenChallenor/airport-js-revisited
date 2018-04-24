03ef2bb00d7414cba3a385b3a86f797a19c1e114
Ben <ben.challenor@btopenworld.com>
2018-04-24 15:41:57 +0100

    Add feature test for plane takeoff

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -14,4 +14,10 @@ describe('Feature test', function() {
     expect(airport.planes()).toContain(plane);
   });
 
+  it('plane can takeoff from an airport', function(){
+    plane.land(airport);
+    plane.takeoff();
+    expect(airport.planes()).not.toContain(plane);
+  });
+
 });
