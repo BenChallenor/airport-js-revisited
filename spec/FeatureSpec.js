fb17087953e46fc5258c083ee271edffdf51cf41
Ben <ben.challenor@btopenworld.com>
2018-04-24 13:12:16 +0100

    Add feature test for landing a plane

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -1,1 +1,17 @@
+'use strict';
 
+describe('Feature test', function() {
+  var airport;
+  var Plane;
+
+  beforeEach(function() {
+    plane = new Plane();
+    airport = new Airport();
+  });
+
+  it('plane can land at an airport', function() {
+    plane.land(airport);
+    expect(airport.planes()).toContain(plane);
+  });
+
+});
